86628aa466a0830695e49376b1a9603326479a68
Dansterhamster <dbyrd38@gatech.edu>
2025-02-05 16:53:50 -0500

    Initial commit

---
+++ iPlay/iPlay/iPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPlayApp.swift
+//  iPlay
+//
+//  Created by Danny Byrd on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct iPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
