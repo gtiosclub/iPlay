4b8ecc63a3cc672adc69ef1559294dd3dddc9e6c
Ross Klaiber <rossk31@icloud.com>
2025-02-06 19:08:28 -0500

    yay new player id

--- iPlay/Multipeer/Player.swift
+++ iPlay/Multipeer/Player.swift
@@ -11,6 +11,7 @@ import MultipeerConnectivity
 struct Player: Hashable, Identifiable {
     //TODO: Add More Attributes: Avatar, ...
     var id: MCPeerID
+    var id_1: MCPeerID
     
     static func ==(a: Player, b: Player) -> Bool {
         a.id == b.id
