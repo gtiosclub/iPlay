ca9611805e32666e120403901f9d89ffc94696c7
Patrick_Ying <patrickyingnb@gmail.com>
2025-03-02 16:58:34 -0500

    Modified the log in page for iphone views, added a empty avatar page, and style the page according to designer's team

--- iPlay/iOS/views/ContentViewiPhone.swift
+++ iPlay/iOS/views/ContentViewiPhone.swift
@@ -11,68 +11,127 @@ import MultipeerConnectivity
 struct ContentViewiPhone: View {
     @State private var username = ""
     @State private var mcManager: MCPlayerManager?
+    @State private var isNavigating = false
     var body: some View {
-        if let mcManager {
-            switch mcManager.viewState {
-            case .preLobby:
-                VStack {
-                    Text("Looking for lobbies...")
-                    List {
-                        ForEach(Array(mcManager.openLobbies)) { lobby in
-                            Section {
-                                HStack {
-                                    Text("\(lobby.id.displayName)'s Lobby")
-                                    Button("Join") {
-                                        if let session = mcManager.session {
-                                            mcManager.browser.invitePeer(lobby.id, to: session, withContext: nil, timeout: 50)
-                                            print("Invited")
-                                        } else {
-                                            print("No session")
+        NavigationStack{
+            if let mcManager {
+                switch mcManager.viewState {
+                case .preLobby:
+                    VStack {
+                        Text("Looking for lobbies...")
+                        List {
+                            ForEach(Array(mcManager.openLobbies)) { lobby in
+                                Section {
+                                    HStack {
+                                        Text("\(lobby.id.displayName)'s Lobby")
+                                        Button("Join") {
+                                            if let session = mcManager.session {
+                                                mcManager.browser.invitePeer(lobby.id, to: session, withContext: nil, timeout: 50)
+                                                print("Invited")
+                                            } else {
+                                                print("No session")
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
                     }
-                }
-            case .inLobby:
-//                InfectedInGameViewiPhone()
-                Button("Send String") {
-                    mcManager.sendPrompt("Hello")
-                }
-                
-            case .inGame:
-                //TODO: Add views for in Game
-                switch mcManager.gameState {
+                case .inLobby:
+                    //                InfectedInGameViewiPhone()
+                    Button("Send String") {
+                        mcManager.sendPrompt("Hello")
+                    }
+                    
+                case .inGame:
+                    //TODO: Add views for in Game
+                    switch mcManager.gameState {
                     case .Infected:
-                    InfectedInGameViewiPhone()
+                        InfectedInGameViewiPhone()
                     case .Spectrum:
                         Text("Spectrum")
+                    }
                 }
-            }
-        } else {
-            VStack {
-                TextField("Username", text: $username)
-                    .padding()
-                    .textFieldStyle(.roundedBorder)
-                    #if os(iOS)
-                    .autocapitalization(.none)
-                    .disableAutocorrection(true)
-                    #endif
+            } else {
+                
+                VStack(spacing:20) {
+                    VStack{
+                        Text("Welcome to\n").font(.system(size: 40))+Text("iPlay").font(.system(size: 40)).fontWeight(.bold)
+                    }
+                    .multilineTextAlignment(.center)
+                    .padding(.top,-130)
+                    
+                    TextField("Username", text: $username)
+                        .padding()
+                    
+                        .frame(width:190,height:50)
+                        .multilineTextAlignment(.center)
+                        .background(
+                            RoundedRectangle(cornerRadius: 20).fill(Color.clear)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+#if os(iOS)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .ignoresSafeArea(.keyboard)
+#endif
                     /*
                      This is needed since we are using a multiplatform app
                      so we need to check if os is iOS for disabling contextual
                      autocapitalization and autocorrect modifiers (QOL)
-                    */
-                Button("Look For Lobbies") {
-                    MCPlayerManager.createSharedInstance(name: username)
-                    mcManager = MCPlayerManager.shared
-                    if let mcManager {
-                        mcManager.viewState = .preLobby
-                        mcManager.start()
-                    } else {
-                        print("MC Manager not initialized")
+                     */
+                    
+//                    Button("Look For Lobbies") {
+//                        MCPlayerManager.createSharedInstance(name: username)
+//                        mcManager = MCPlayerManager.shared
+//                        if let mcManager {
+//                            mcManager.viewState = .preLobby
+//                            mcManager.start()
+//                        } else {
+//                            print("MC Manager not initialized")
+//                        }
+//                    }
+                    Button(action: {
+                        MCPlayerManager.createSharedInstance(name: username)
+                        mcManager = MCPlayerManager.shared
+                        if let mcManager {
+                            mcManager.viewState = .preLobby
+                            mcManager.start()
+                        } else {
+                            print("MC Manager not initialized")
+                        }
+                    }) {
+                        Text("Join game")
+                            .font(.title2)
+                            .foregroundColor(.white) // Text color
+                            .frame(width: 190, height: 50) // Size of button
+                            .background( // Button background color
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .stroke(Color.black, lineWidth: 1).fill(Color.black)
+
+                            )
+                            
+                        
                     }
+                    
+                    Button(action: {
+                        isNavigating = true
+                    }) {
+                        Text("Avatar")
+                            .font(.title2)
+                            .foregroundColor(.black) // Text color
+                            .frame(width: 190, height: 50) // Size of button
+                            .background(
+                                RoundedRectangle(cornerRadius: 20).stroke( Color.black,lineWidth:1).fill(Color.gray)
+                            ) // Button background color
+                            
+                    }
+                    .navigationDestination(isPresented:$isNavigating){
+                        AvatarView()
+                        
+                    }
+                    
+                    
                 }
             }
         }
